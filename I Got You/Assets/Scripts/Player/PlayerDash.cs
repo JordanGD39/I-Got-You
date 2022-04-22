160879d394a2b9e55b5214d4face3b22fbfb21d1
Everardus Tol <43673344+jobtolmop@users.noreply.github.com>
2022-04-22 13:03:32 +0200

    Implemented Dash Mechanic

---
+++ I Got You/Assets/Scripts/Player/PlayerDash.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+ 
+    }
+ 
+}
--- I Got You/Assets/Scripts/Player/PlayerMovement.cs
+++ I Got You/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,9 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runSpeed = 12;
     [SerializeField] private float gravity = -9.8f;
     [SerializeField] private float jumpHeight = 10;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private Vector3 movement;
     private float stepOffset = 0.5f;
 
     private Vector3 velocity = Vector3.zero;
@@ -24,14 +27,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (DashCheck =)
+        //{
+        //    UpdateMovement();
+        //    als de dashcheck aan staat moet updatemovement uit.
+        //    
+        //}
         UpdateMovement();
         MoveByGravity();
         JumpCheck();
+        DashCheck();
     }
 
     private void UpdateMovement()
     {
-        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         movement.Normalize();
         movement = transform.TransformDirection(movement);
         movement.y = 0;
@@ -63,6 +73,27 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && characterController.isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void DashCheck()
+    {
+        if (Input.GetButtonDown("Dash") && characterController.isGrounded)
+        {
+            StartCoroutine(Dash());
+            Debug.Log("Dash");
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+
+        while (Time.time < startTime + dashTime)
+        {
+            characterController.Move(movement * dashSpeed * Time.deltaTime);
+
+            yield return null;
         }
     }
 }
