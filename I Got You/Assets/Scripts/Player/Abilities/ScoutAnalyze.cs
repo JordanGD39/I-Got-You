1a135da3e746e42d68b079a9ce9ba77330388132
JordanG <26260@ma-web.nl>
2022-04-28 00:22:02 +0200

    Scout analyze beginning added

---
+++ I Got You/Assets/Scripts/Player/Abilities/ScoutAnalyze.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoutAnalyze : MonoBehaviour
+{
+    private GameObject analyzeVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        analyzeVolume = GameObject.FindGameObjectWithTag("ScoutVolume").transform.GetChild(0).gameObject;
+        analyzeVolume.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckAbilityToggle();
+    }
+
+    public void CheckAbilityToggle()
+    {
+        if (Input.GetButtonDown("Ability"))
+        {
+            analyzeVolume.SetActive(!analyzeVolume.activeSelf);
+        }
+    }
+}
--- I Got You/Assets/Scripts/Player/AbilityAdder.cs
+++ I Got You/Assets/Scripts/Player/AbilityAdder.cs
@@ -1,14 +1,9 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class PassiveApplyer : MonoBehaviourPun
+public class AbilityAdder : MonoBehaviourPun
 {
-    [SerializeField] private float scoutSpeedBuff = 1.1f;
-    [SerializeField] private float supportHealBuff = 1.5f;
-    [SerializeField] private float bomberDamageNerf = 0.75f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +14,21 @@ public class PassiveApplyer : MonoBehaviourPun
 
         PlayerStats playerStats = GetComponent<PlayerStats>();
 
+        if (playerStats.CurrentClass != PlayerStats.ClassNames.SCOUT)
+        {
+            GameObject.FindGameObjectWithTag("ScoutVolume").SetActive(false);
+        }
+
         switch (playerStats.CurrentClass)
         {
             case PlayerStats.ClassNames.SCOUT:
-                GetComponent<PlayerMovement>().ModifySpeedStats(scoutSpeedBuff);
+                gameObject.AddComponent<ScoutAnalyze>();
                 break;
             case PlayerStats.ClassNames.TANK:
-                playerStats.HasShieldHealth = true;
                 break;
             case PlayerStats.ClassNames.SUPPORT:
-                GetComponent<PlayerHealing>().ModifyHealingStat(supportHealBuff);
                 break;
             case PlayerStats.ClassNames.BOMBER:
-                GetComponent<PlayerShoot>().ModifyDamageStat(bomberDamageNerf);
                 break;
         }
     }
